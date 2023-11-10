fa618409cd78925d300ba93baecb52850cf904bb
abeed1772 <128729270+abeed1772@users.noreply.github.com>
2023-11-10 19:48:33 +0530

    Update index.jsx

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <section id="home" className="home">
       <div className="home__text-wrapper">
         <h1>
-          Hello, I'm Jafer
+          Hello, I'm Abeed
           <br />
           Front end developer
         </h1>
